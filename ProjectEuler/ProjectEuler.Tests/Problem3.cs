7e49ebae8977668072db6c1e183ad6c65ff5cfae
Rose,Nathan <nathan.p.rose@gmail.com>
2019-04-21 11:20:52 -0600

    Problems 1-2

---
+++ ProjectEuler/ProjectEuler.Tests/Problem3.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectEuler.Tests
+{
+    [TestClass]
+    public class Problem3
+    {
+        /// <summary>
+        /// The prime factors of 13195 are 5, 7, 13 and 29.
+        ///
+        /// What is the largest prime factor of the number 600851475143 ?
+        /// </summary>
+        [TestMethod]
+        public void Run()
+        {
+            long largestFactor = 0;
+
+            Assert.AreEqual(largestFactor, 6857);
+        }
+    }
+}
